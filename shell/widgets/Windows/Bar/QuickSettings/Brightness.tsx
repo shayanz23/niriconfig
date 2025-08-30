839b7b2ae6ebfcac043bc0dddcbe79f6608aa6cf
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2025-08-30 11:21:53 -0700

    Fixed brightness slider hopefully

--- shell/widgets/Windows/Bar/QuickSettings/Brightness.tsx
+++ shell/widgets/Windows/Bar/QuickSettings/Brightness.tsx
@@ -5,24 +5,32 @@ import { createPoll } from "ags/time"
 import Wp from "gi://AstalWp"
 
 export default function Brightness() {
+    let MAX = 0
     let exists = true
     try {
+        MAX = Number(exec("brightnessctl m"))
         exec("bash -c 'brightnessctl -l | grep leds'")
     } catch (error) {
         exists = false
     }
     const brightness = createPoll("", 5000, "brightnessctl g")
 
+    if (exists) {
+        return [
+            <box/>
+        ]
+    }    
+
     return (
         <box>
             <With value={brightness}>
                 {(value) =>
-                    <box cssName={"brightness-box"} visible={exists}>
+                    <box cssName={"brightness-box"}>
                         <button cssClasses={["brightness-button"]}>
                             <image iconName={"display-brightness-medium-symbolic"} />
                         </button>
-                        <slider cssClasses={["menu-slider"]} widthRequest={295} value={Number(value)/255} onValueChanged={(self) => {
-                            execAsync(`brightnessctl s ${self.value*255}`)
+                        <slider cssClasses={["menu-slider"]} widthRequest={295} value={Number(value)/MAX} onValueChanged={(self) => {
+                            execAsync(`brightnessctl s ${self.value*MAX}`)
                         }} />
                     </box>
                 }
