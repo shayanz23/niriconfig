c5704d71e090bd613374f6862ec9694ececa0f3d
Shayan Zahedanaraki <shayan.zahedana@gmail.com>
2025-11-11 21:23:52 -0800

    improved theming further by adding papirus support.

--- rofi/powermenu/run-power-menu.sh
+++ rofi/powermenu/run-power-menu.sh
@@ -3,5 +3,5 @@ if pgrep -x "rofi" > /dev/null
 then
     pkill -x "rofi"
 else
-    echo "suspend|shutdown|logout|restart|" | rofi -sep '|' -dmenu -p "power options" | bash ~/.config/rofi/powermenu/actions.sh
+    echo -e "suspend\nshutdown\nlogout\nrestart" | rofi -dmenu -p "power options" | bash ~/.config/rofi/powermenu/actions.sh
 fi
--- themes/set-themes.sh
+++ themes/set-themes.sh
@@ -5,8 +5,10 @@ echo $piped_data
 
 if [ "$piped_data" = "light" ]
     cp -f ~/.config/themes/themes/$piped_data/_index.scss ~/.config/themes/_index.scss
+    gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
 else if [ "$piped_data" = "dark" ]
     cp -f ~/.config/themes/themes/$piped_data/_index.scss ~/.config/themes/_index.scss
+    gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
 else
     echo "invalid option."
 end
